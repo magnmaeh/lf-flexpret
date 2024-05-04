4b5b1f5b805d564da3c1637ae722b4c6c6189a3e
Magnus Mæhlum <magnusmaehlum@outlook.com>
2024-05-04 16:09:37 +0200

    Update flexpret and LF repos to work-in-progress flexpret support integrated in LF.

--- env.bash
+++ env.bash
@@ -1,2 +1,6 @@
-export FLEXPRET_ROOT_DIR="$(pwd)/flexpret"
-export PATH="$PATH:$(pwd)/flexpret/emulator"
+#!/usr/bin/env bash
+
+# From https://stackoverflow.com/questions/59895/how-do-i-get-the-directory-where-a-bash-script-is-located-from-within-the-script 
+LF_FLEXPRET_ROOT=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+source $LF_FLEXPRET_ROOT/flexpret/env.bash
--- env.fish
+++ env.fish
@@ -1,2 +1,4 @@
-set FLEXPRET_ROOT_DIR (pwd)/flexpret
-set PATH $PATH:(pwd)/flexpret/emulator
+# From: https://stackoverflow.com/questions/19672673/can-a-fish-script-tell-what-directory-its-stored-in
+LF_FLEXPRET_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+source $LF_FLEXPRET_ROOT/flexpret/env.fish
